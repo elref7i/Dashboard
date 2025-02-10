e2b7fe6e404f012d3203783215a9004b2b578dde
elref7i <momo3956248@gmail.com>
2025-02-10 04:43:52 +0200

    refactor: enhance Vite build configuration with minification and tree-shaking

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    minify: 'esbuild',
+    sourcemap: false,
+    treeshake: true,
+    esbuild: {
+      minify: true,
+      treeShaking: true,
+    },
+  },
 });
