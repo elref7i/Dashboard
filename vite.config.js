98a17b89914be8ee7206acb385ae1fadb6434b89
elref7i <momo3956248@gmail.com>
2025-02-10 04:41:34 +0200

    refactor: simplify Vite configuration and clean up main.jsx imports

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
-  base: './', // أو جرب "/"
-  build: {
-    minify: 'esbuild',
-    sourcemap: false,
-    treeshake: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.includes('@mui') ? 'mui' : 'vendor';
-          }
-        },
-      },
-    },
-    esbuild: {
-      minify: true,
-      treeShaking: true,
-    },
-  },
+  plugins: [react()],
 });
